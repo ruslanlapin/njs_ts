885536bab82c9959a57ab63a6d82e555c37f9f95
Ruslan <ruslanlapin@yandex.ru>
2024-05-30 20:16:28 +0300

    Commit - 3 object kod

---
+++ src/02/02.ts
@@ -0,0 +1,58 @@
+type CityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType ={
+    streetTitle: string
+    city: CityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type studentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: studentType = {
+    id: 1,
+    name: "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
+
+console.log(student.name)
+console.log(student.age)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
+
+
